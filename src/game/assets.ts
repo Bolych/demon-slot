dba0b465fa55c43cae477920a3ca91add408a888
aleksandr bolychevskiy <alexbolych64@gmail.com>
2025-11-16 16:09:40 +0400

    asset path for gh

--- src/game/assets.ts
+++ src/game/assets.ts
@@ -24,16 +24,16 @@ export async function loadGameAssets(): Promise<GameAssets> {
 
   const [background, board, symbol1, symbol2, symbol3, symbol4, symbol5, playButton, winInfo, logo] =
     await Promise.all([
-      Assets.load('/assets/ui/bg-enigma-castle.webp'),
-      Assets.load('/assets/ui/board.webp'),
-      Assets.load('/assets/symbols/symbol-helmet.webp'),
-      Assets.load('/assets/symbols/symbol-fire.webp'),
-      Assets.load('/assets/symbols/shield.webp'),
-      Assets.load('/assets/symbols/demon.webp'),
-      Assets.load('/assets/symbols/sword.webp'),
-      Assets.load('/assets/ui/spin.webp'),
-      Assets.load('/assets/ui/win-info.webp'),
-      Assets.load('/assets/ui/logo-hunt-1.png'),
+      Assets.load(`${import.meta.env.BASE_URL}assets/ui/bg-enigma-castle.webp`),
+      Assets.load(`${import.meta.env.BASE_URL}assets/ui/board.webp`),
+      Assets.load(`${import.meta.env.BASE_URL}assets/symbols/symbol-helmet.webp`),
+      Assets.load(`${import.meta.env.BASE_URL}assets/symbols/symbol-fire.webp`),
+      Assets.load(`${import.meta.env.BASE_URL}assets/symbols/shield.webp`),
+      Assets.load(`${import.meta.env.BASE_URL}assets/symbols/demon.webp`),
+      Assets.load(`${import.meta.env.BASE_URL}assets/symbols/sword.webp`),
+      Assets.load(`${import.meta.env.BASE_URL}assets/ui/spin.webp`),
+      Assets.load(`${import.meta.env.BASE_URL}assets/ui/win-info.webp`),
+      Assets.load(`${import.meta.env.BASE_URL}assets/ui/logo-hunt-1.png`),
     ]);
 
   loadedAssets = {
